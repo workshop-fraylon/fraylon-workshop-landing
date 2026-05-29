19fe410ad62e7bd25f78a17992aa18be9aceac54
Bhumika_Dolnare01 <dolnarebhumika@gmail.com>
2026-05-29 09:35:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const Razorpay = require("razorpay");
+require("dotenv").config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+app.post("/create-order", async (req, res) => {
+  try {
+    const options = {
+      amount: 99900, // ₹999 in paise
+      currency: "INR",
+      receipt: "receipt_order_1",
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+app.use(express.static(__dirname));
